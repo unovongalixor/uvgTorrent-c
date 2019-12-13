b1b5b493b328c9537e959016b9f3d64ce30b5d07
Von Galixor <smnbursten@gmail.com>
2019-12-13 14:04:32 +0200

    moved mocked functions into seperate file

---
+++ test/mocked_functions.c
@@ -0,0 +1,30 @@
+
+
+// set USE_REAL_MALLOC to 0 to return mocked value
+int USE_REAL_MALLOC = 1;
+void * __real_malloc( size_t size );
+void * __wrap_malloc( size_t size )
+{
+  if (USE_REAL_MALLOC == 1) {
+    return __real_malloc( size );
+  } else {
+    return (void *) mock();
+  }
+}
+
+// set USE_REAL_STRNDUP to 0 to return mocked value
+int USE_REAL_STRNDUP = 1;
+char* __real_strndup(const char *s, size_t n);
+void* __wrap_strndup(const char *s, size_t n)
+{
+      if (USE_REAL_STRNDUP == 0) {
+          return (char *) mock();
+      } else {
+         return __real_strndup(s, n);
+      }
+}
+
+void reset_mocks() {
+  USE_REAL_MALLOC = 1;
+  USE_REAL_STRNDUP = 1;
+}

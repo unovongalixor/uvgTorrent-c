da4a84caf922dd3715413b552fde1f027b321548
Von Galixor <smnbursten@gmail.com>
2019-12-16 22:21:34 +0200

    added basic threadpool header

--- src/thread_pool/thread_pool.h
+++ src/thread_pool/thread_pool.h
@@ -6,10 +6,11 @@
 struct Queue {
   pthread_mutex_t * mutex;
   StsHeader * queue;
+  int count;
 };
 
 struct Job {
-  void (*func)(struct Queue *, ...);
+  void (*func)(struct Queue *, int arg_count, ...);
   struct Queue * result_queue;
   void * args[];
 };
@@ -32,14 +33,24 @@ struct ThreadPool {
 extern struct ThreadPool * thread_pool_new(int thread_count);
 
 /**
-* extern struct ThreadPool * thread_pool_new(int thread_count)
+* extern struct ThreadPool * thread_pool_free(struct ThreadPool * tp)
 *
 * int thread_count  : maximum number of threads
 *
 * NOTES   : mallocs a new ThreadPool struct
 * RETURN  : struct ThreadPool *
 */
-extern struct ThreadPool * thread_pool_new(int thread_count);
+extern struct ThreadPool * thread_pool_free(struct ThreadPool * tp);
+
+/**
+* extern int thread_pool_add_job(struct Job *)
+*
+* struct Job *;
+*
+* NOTES   : add a job to the given ThreadPools job queue
+* RETURN  : success
+*/
+extern int thread_pool_add_job(struct Job * j);
 
 /**
 * extern struct Queue * queue_new()
@@ -50,4 +61,33 @@ extern struct ThreadPool * thread_pool_new(int thread_count);
 */
 extern struct Queue * queue_new();
 
-#endif // UVGTORRENT_C_TORRENT_H
+/**
+* void queue_push(void *elem)
+*
+* void *elem;
+*
+* NOTES   : push elem into the queue
+* RETURN  :
+*/
+void queue_push(void *elem);
+
+/**
+* void * queue_pop()
+*
+* NOTES   : pop elem from queue
+* RETURN  : void *
+*/
+void * queue_pop();
+
+/**
+* extern struct Queue * queue_free(struct Queue *)
+*
+* struct Queue *;
+*
+* NOTES   : clean up the torrent and all associated tracker objects
+* RETURN  : freed and NULL'd struct Torrent *
+*/
+extern struct Queue * queue_free(struct Queue *);
+
+
+#endif // UVGTORRENT_C_THREAD_POOL_H

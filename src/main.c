19556805fe961487f41441c5ee6188d37cad105e
Von Galixor <smnbursten@gmail.com>
2019-12-16 22:45:21 +0200

    remove main comment

--- src/main.c
+++ src/main.c
@@ -1,19 +1,3 @@
-/*
- * ============================================================================
- *
- *       Filename:  main.c
- *
- *    Description:  Main file of the project
- *
- *        Created:  03/24/2016 19:40:56
- *
- *         Author:  Gustavo Pantuza
- *   Organization:  Software Community
- *
- * ============================================================================
- */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 

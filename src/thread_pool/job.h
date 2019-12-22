d3de98cbb7adca039c694e7edeb26a22123be707
Simon Bursten <simon@breezometer.com>
2019-12-22 17:54:58 +0200

    cleanup and comment thread_pool job

--- src/thread_pool/job.h
+++ src/thread_pool/job.h
@@ -6,16 +6,17 @@
 #include <pthread.h>
 
 struct Job {
-  int (*execute)(int * cancel_flag, struct Queue * result_queue, ...);
-  struct Queue * result_queue;
-  int arg_count;
-  size_t arg_size;
-  void * args[];
+    int (*execute)(int *cancel_flag, struct Queue *result_queue, ...);
+
+    struct Queue *result_queue;
+    int arg_count;
+    size_t arg_size;
+    void *args[];
 };
 
 
 /**
-* extern struct Job * job_new(int (*execute)(struct Queue *, ...), struct Queue * result_queue, int arg_count, void* args[])
+* extern struct Job * job_new(int (*execute)(int * cancel_flag, struct Queue * result_queue, ...), struct Queue * result_queue, int arg_count, void* args[])
 *
 * int (*execute)(struct Queue *, ...)  : pointer to function to execute
 * struct Queue * result_queue          : queue to return results
@@ -25,17 +26,20 @@ struct Job {
 * NOTES   : any memory allocated from this function becomes the responsibility of the owner of the results_queue
 * RETURN  : struct Job *
 */
-extern struct Job * job_new(int (*execute)(int * cancel_flag, struct Queue * result_queue, ...), struct Queue * result_queue, int arg_count, void* args[]);
+extern struct Job *
+job_new(int (*execute)(int *cancel_flag, struct Queue *result_queue, ...), struct Queue *result_queue, int arg_count,
+        void *args[]);
 
 /**
-* int job_execute(struct Job * j)
+* int job_execute(struct Job * j, int * cancel_flag)
 *
 * struct Job * j;
+* int * cancel_flag: pointer to a cancel flag. check to see if you need to gracefully exit
 *
 * NOTES   : executes the job
 * RETURN  : success
 */
-int job_execute(struct Job * j, int * cancel_flag);
+int job_execute(struct Job *j, int *cancel_flag);
 
 /**
 * extern struct Job * job_free(struct Job * j)
@@ -45,6 +49,6 @@ int job_execute(struct Job * j, int * cancel_flag);
 * NOTES   : frees a job
 * RETURN  : struct Job *
 */
-extern struct Job * job_free(struct Job * j);
+extern struct Job *job_free(struct Job *j);
 
 #endif

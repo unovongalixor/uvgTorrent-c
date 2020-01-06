159cb2cceeadbc5908967bd3d9733bbe0ac8b119
Von Galixor <smnbursten@gmail.com>
2020-01-06 20:30:22 +0200

    added hashmap get and new function

--- src/hash_map/hash_map.c
+++ src/hash_map/hash_map.c
@@ -1,4 +1,77 @@
-//
-// Created by vongalixor on 1/4/20.
-//
+#include "../macros.h"
+#include "hash_map.h"
+#include <inttypes.h>
+#include <stdlib.h>
 
+/* PRIVATE FUNCTIONS */
+// https://en.wikipedia.org/wiki/Jenkins_hash_function
+uint32_t jenkins_one_at_a_time_hash(const char * key, size_t length) {
+    size_t i = 0;
+    uint32_t hash = 0;
+    while (i != length) {
+        hash += key[i++];
+        hash += hash << 10;
+        hash ^= hash >> 6;
+    }
+    hash += hash << 3;
+    hash ^= hash >> 11;
+    hash += hash << 15;
+    return hash;
+}
+
+/* PUBLIC FUNCTIONS */
+struct HashMap * hashmap_new(int max_buckets) {
+    struct HashMap * hm = NULL;
+
+    size_t size = sizeof(struct HashMap) + (sizeof(struct HashMapItem) * max_buckets);
+    hm = malloc(size);
+    if (hm == NULL) {
+        throw("hashmap failed to alloc");
+    }
+    hm->max_buckets = max_buckets;
+
+    return hm;
+
+    error:
+    return hm;
+}
+
+void * hashmap_get(struct HashMap * hm, char * key) {
+    uint32_t hash = jenkins_one_at_a_time_hash(key, (size_t) strlen(key));
+    int index = hash % hm->max_buckets;
+
+    struct HashMapItem * item = hm->buckets[index];
+
+    while (item != NULL) {
+        if (strcmp(key, item->key) == 1) {
+            return item;
+        }
+        item = item->next;
+    }
+
+    return item;
+}
+
+/**
+ * @brief check if a key is set in the given hashmap
+ * @param hm
+ * @param key
+ * @return 1 or 0
+ */
+extern int hashmap_has_key(struct HashMap * hm, char * key);
+
+/**
+ * @brief add an element to the hash map
+ * @param hm
+ * @param key
+ * @param value
+ * @return
+ */
+extern int hashmap_set(struct HashMap * hm, char * key, void * value);
+
+/**
+ * @brief free the given hashmap
+ * @param hm
+ * @return struct HashMap *. NULL on success
+ */
+extern struct HashMap * hashmap_free(struct HashMap * hm);
